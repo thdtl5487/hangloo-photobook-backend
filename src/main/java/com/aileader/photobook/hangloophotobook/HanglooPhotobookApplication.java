d612db15c0131bf471dffd2e59cf10dacf7f38ff
thdtl5487 <thdtl5487@gmail.com>
2022-10-26 15:15:06 +0900

    first commit

---
+++ src/main/java/com/aileader/photobook/hangloophotobook/HanglooPhotobookApplication.java
@@ -0,0 +1,13 @@
+package com.aileader.photobook.hangloophotobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanglooPhotobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanglooPhotobookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aileader/photobook/hangloophotobook/HanglooPhotobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aileader.photobook.hangloophotobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanglooPhotobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
