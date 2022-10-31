54de79e6a7cad45500e20b8bcd2f7512f8b10fb2
thdtl5487 <thdtl5487@gmail.com>
2022-10-31 13:20:53 +0900

    교체된 프로젝트 업로드2

---
+++ src/main/java/kr/co/hangloo/hangloo/HanglooApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hangloo.hangloo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanglooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanglooApplication.class, args);
+	}
+
+}
