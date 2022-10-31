8cedc4bf61c66d317cba31c15f56197062f38e08
thdtl5487 <thdtl5487@gmail.com>
2022-10-31 13:12:51 +0900

    교체된 프로젝트 업로드

---
+++ src/test/java/com/photobook/photobook/PhotobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photobook.photobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
